48290216ffe0fa0941ebf69714947a42dd962836
XingChao <1363688001@qq.com>
2022-09-27 15:56:12 +0800

    sh

--- start.sh
+++ start.sh
@@ -2,8 +2,9 @@
 rm -rf /opt/gitee/python/django_blog_tutorial/rumor8008.log
 
 cd /opt/gitee/python/django_blog_tutorial
+
 git reset --hard
 git pull
 
-cd /opt/gitee/python/django_blog_tutorial
+source venv/bin/activate
 /usr/bin/nohup python manage.py runserver 0.0.0.0:8008 >> /opt/gitee/python/django_blog_tutorial/rumor8008.log 2>&1 &
